28f63d1d51b5ec3a1ea7506e8b57d763fc174f55
Lenin Khangjrakpam <leninkhangjrakpam@gmail.com>
2024-10-12 20:08:17 +0530

    add language icon to card

--- src/components/RepoCard.tsx
+++ src/components/RepoCard.tsx
@@ -4,10 +4,19 @@ import { Card } from "./ui/card";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import Link from "next/link";
 import { Button } from "./ui/button";
-import { BugIcon, ForkIcon, StarIcon } from "./ui/icons";
+import { BugIcon, ForkIcon, StarIcon, CodeIcon } from "./ui/icons";
+import { PROGRAMMING_LANGUAGES } from "@/utils/constants";
 
 const RepoCard = ({ data }: any) => {
 	const [showMore, setShowMore] = useState(false);
+
+	const LangIcon:
+		| React.ComponentType<{ width: number; height: number; fill: string }>
+		| undefined = PROGRAMMING_LANGUAGES.find(
+		(d) => d.iconName.toLowerCase() === data.language.toLowerCase(),
+	)?.icon as
+		| React.ComponentType<{ width: number; height: number; fill: string }>
+		| undefined;
 
 	return (
 		<Card className="w-full max-w-sm p-6 grid gap-6">
@@ -19,6 +28,14 @@ const RepoCard = ({ data }: any) => {
 				<div className="space-y-1 w-fit px-2">
 					<h3 className="text-xl font-bold w-full">{data.name}</h3>
 					<p className="text-muted-foreground">{data.owner.login}</p>
+					<div className="flex flex-row items-center gap-1">
+						{LangIcon ? (
+							<LangIcon width={24} height={24} fill="currentColor" />
+						) : (
+							<CodeIcon width={24} height={24} fill="currentColor" />
+						)}
+						<div>{data.language}</div>
+					</div>
 				</div>
 			</div>
 			<div className="space-y-4 text-sm min-w-fit">
@@ -90,31 +107,5 @@ const RepoCard = ({ data }: any) => {
 		</Card>
 	);
 };
-// function BugIcon(props: any) {
-// 	return (
-// 		<svg
-// 			{...props}
-// 			xmlns="http://www.w3.org/2000/svg"
-// 			width="24"
-// 			height="24"
-// 			viewBox="0 0 24 24"
-// 			fill="none"
-// 			stroke="currentColor"
-// 			strokeWidth="2"
-// 			strokeLinecap="round"
-// 			strokeLinejoin="round">
-// 			<path d="m8 2 1.88 1.88" />
-// 			<path d="M14.12 3.88 16 2" />
-// 			<path d="M9 7.13v-1a3.003 3.003 0 1 1 6 0v1" />
-// 			<path d="M12 20c-3.3 0-6-2.7-6-6v-3a4 4 0 0 1 4-4h4a4 4 0 0 1 4 4v3c0 3.3-2.7 6-6 6" />
-// 			<path d="M12 20v-9" />
-// 			<path d="M6.53 9C4.6 8.8 3 7.1 3 5" />
-// 			<path d="M6 13H2" />
-// 			<path d="M3 21c0-2.1 1.7-3.9 3.8-4" />
-// 			<path d="M20.97 5c0 2.1-1.6 3.8-3.5 4" />
-// 			<path d="M22 13h-4" />
-// 			<path d="M17.2 17c2.1.1 3.8 1.9 3.8 4" />
-// 		</svg>
-// 	);
-// }
+
 export default RepoCard;
